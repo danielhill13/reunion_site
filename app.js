20dfb45904af7e8f065acb36b9d3459564e16d7f
danielhill13 <daniel.hill13@gmail.com>
2017-04-24 11:00:46 -0700

    added bootstrap and jquery to header.ejs

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 var express = require('express'),
     app = express();
 
- app.set("view engine", "ejs");
+
+//CONFIG
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res){
+    res.render('index');
+});
+
+
+
+app.listen(3000, function(req, res){
+    console.log("Family Server Started");
+});
