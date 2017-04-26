3334040fad72370a8bd89d6c4bcadd7a070ba68d
danielhill13 <daniel.hill13@gmail.com>
2017-04-26 14:44:33 -0700

    RESTful routes for destinations DONE

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = express(),
-    mongoose = require("mongoose")
-    bodyParser = require("body-parser")
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
     methodOverride = require("method-override");
 
 
@@ -15,6 +15,7 @@ app.use(methodOverride("_method"));
 
 var destinationSchema = new mongoose.Schema({
     location: String,
+    image: String,
     url: String,
     body: String,
     submittedBy: String,
@@ -25,23 +26,10 @@ var Destination = mongoose.model("Destination", destinationSchema);
 app.get("/", function(req, res){
     res.render('index');
 });
-app.get("/event-details", function(req, res){
-    res.render('event-details');
-});
 
-app.get("/photos", function(req, res){
-    res.render('photos');
-});
-app.get("/guestbook", function(req, res){
-    res.render('guestbook');
-});
-app.get("/rsvp", function(req, res){
-    res.render('rsvp');
-});
-app.get("/signup", function(req, res){
-    res.render('signup');
-});
 
+
+//DESTINATIONS AND ALL THE RESTful ROUTES
 app.get("/destinations", function(req, res){
     Destination.find({}, function(err, destinations){
         if(err){
@@ -52,7 +40,6 @@ app.get("/destinations", function(req, res){
     });
 });
 
-//RESTFUL ROUTES DESTINATIONS
 //NEW
 app.get("/destinations/new", function(req, res){
     res.render("newdestination");
@@ -90,7 +77,44 @@ app.get("/destinations/:id/edit", function(req, res){
     //prepopulate forms
 });
 //UPDATE
+app.put("/destinations/:id", function(req, res){
+    Destination.findByIdAndUpdate(req.params.id, req.body.destination, function(err, updatedDestination){
+        if(err){
+            console.log("Error Updating Destination");
+            res.redirect("/destinations");
+        } else {
+            res.redirect("/destinations/" + req.params.id);
+        }
+    });
+});
 //DESTROY
+app.delete("/destinations/:id", function(req, res){
+    Destination.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            console.log("Issue deleting object");
+            res.redirect("/destinations");
+        } else{
+            res.redirect("/destinations");
+        }
+    })
+});
+
+app.get("/event-details", function(req, res){
+    res.render('event-details');
+});
+app.get("/photos", function(req, res){
+    res.render('photos');
+});
+app.get("/guestbook", function(req, res){
+    res.render('guestbook');
+});
+app.get("/rsvp", function(req, res){
+    res.render('rsvp');
+});
+app.get("/signup", function(req, res){
+    res.render('signup');
+});
+
 
 app.listen(3000, function(req, res){
     console.log("Family Server Started");
